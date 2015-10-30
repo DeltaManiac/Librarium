3c47cc9f551e2493da970fc1babf8f7a4fbc9df0
Harikrishnan Menon <harikrishnan.menon@sap.com>
2015-10-30 10:10:37 +0530

    Ui Fixes again

--- app/views/user/upload.blade.php
+++ app/views/user/upload.blade.php
@@ -5,31 +5,18 @@
 
 <div class="container center-block login vertical-centre">
     {{ Form::open(array('files'=>true,'route'=>'book.store')) }}
-            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            {{--<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>--}}
      <div class="form-group">
-    {{ Form::label('username','Title ',array('id'=>'','class'=>'     text-center')) }}
-    {{ Form::text("BookName") }}
+    {{ Form::label('username','Title',array('id'=>'','class'=>'text-center fonts')) }}
+    {{ Form::text("BookName",'',array('class'=>'form-control text-center')) }}
      </div>
-    {{ Form::label('username','Author : ',array('id'=>'','class'=>'')) }}
-    {{ Form::text("Author") }}
+    {{ Form::label('username','Author',array('id'=>'author','class'=>'')) }}
+    {{ Form::text("Author",'',array('class'=>'form-control text-center'))}}
     {{--{{ Form::label('fileName', 'Select Book PDF') }}--}}
-    {{--<span class="btn btn-default btn-file">--}}
-    {{--{{ Form::file('file','',array('id'=>'','class'=>'loginBtn')) }}--}}
-    {{--<span class="btn btn-default btn-file">--}}
-        {{--Browse <input type="file">--}}
-    {{--</span>--}}
-    {{----}}
 
-<div style="position:relative;">
-        <a class='btn btn-primary' href='javascript:;'>
-            Choose File...
-            <input type="file" style='padding-top: 17px;position:absolute;z-index:2;top:0;left:0;filter: alpha(opacity=0);-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";opacity:0;background-color:transparent;color:transparent;' name="file_source" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
-        </a>
-        &nbsp;
-        <span class='label label-info text-center' id="upload-file-info"></span>
-</div>
-    {{--</span>--}}
-    {{--{{ F    orm::label('image', 'Select Image') }}--}}
+    {{ Form::file('file','',array('id'=>'','class'=>'file')) }}
+
+    {{--{{ Form::label('image', 'Select Image') }}--}}
     {{ Form::file('image') }}
     {{ Form::submit('Save') }}
 
