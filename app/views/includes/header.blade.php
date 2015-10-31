f104cb7085c4e673bfb81d10e76a324f82d29e24
Harikrishnan Menon <harikrishnan.menon@sap.com>
2015-10-31 09:35:20 +0530

    Ui Fixes again

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -7,17 +7,24 @@
       <ul class="nav navbar-nav navbar-right">
         {{--<li class="active"><a href="#"></a></li>--}}
          @if(Auth::user())
-              <li><a href="{{ URL::to('profile') }}"><span class="glyphicon glyphicon-th"></span> Books </a></li>
               <li>
-                  <form id="search-form" class="navbar-form" action="/search" method="get" role="search">
-                      <div class="input-group">
-                          <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-                          <div class="input-group-btn">
-                              <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                          </div>
-                      </div>
-                  </form>
+                  {{--<form id="search-form" class="navbar-form" action="/search" method="get" role="search">--}}
+                      {{--<div class="input-group">--}}
+                          {{--<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">--}}
+                          {{--<div class="input-group-btn">--}}
+                              {{--<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>--}}
+                          {{--</div>--}}
+                      {{--</div>--}}
+                  {{--</form>--}}
+                  <form action="" class="search-form">
+                                  <div class="form-group has-feedback">
+                              		<label for="search" class="sr-only">Search</label>
+                              		<input type="text" class="form-control" name="search" id="search" placeholder="search">
+                                		<span class="glyphicon glyphicon-search form-control-feedback"></span>
+                              	</div>
+                              </form>
               </li>
+             <li><a href="{{ URL::to('profile') }}"><span class="glyphicon glyphicon-th"></span> Books </a></li>
             <li><a href="{{ URL::to('upload') }}"><span class="glyphicon glyphicon-cloud-upload	"></span> Upload </a></li>
             <li><a href="{{ URL::to('logout') }}"><span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
           @else
